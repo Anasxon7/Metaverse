2fec2ccf189fd580ab0b3de7a60150afcd381780
Madaminov <muhammadanasxon6694@gmail.com>
2023-01-28 22:26:06 +0500

    add

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,15 @@ import styles from "../styles";
 import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
+  <motion.nav
+    variants={{ navVariants }}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" ></div>
 
+  </motion.nav>
 );
 
 export default Navbar;
