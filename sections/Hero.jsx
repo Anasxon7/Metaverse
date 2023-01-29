345022b7f9fcfec490153492763067a3b16f39d2
Madaminov <muhammadanasxon6694@gmail.com>
2023-01-30 00:10:27 +0500

    add hero

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -14,7 +14,19 @@ const Hero = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-
+      <div className="relative z-10 flex flex-col items-center justify-center">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Metaverse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row items-center justify-center"
+        >
+          <h1 className={styles.heroHeading}>Ma</h1>
+          <div className={styles.heroDText} />
+          <h1 className={styles.heroHeading}>Ness</h1>
+        </motion.div>
+      </div>
 
       <motion.div
         variants={slideIn('right', 'tween', 0.2, 1)}
