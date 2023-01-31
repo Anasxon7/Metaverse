9d34ad483ab3ec5cfad308cdeeaf961e9df038c2
Madaminov <muhammadanasxon6694@gmail.com>
2023-02-01 00:14:18 +0500

    add about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -39,7 +39,11 @@ const About = () => (
       <motion.img
       variants={fadeIn( 'up', 'tween', 0.3, 1)}
       src="/arrow-down.svg"
-
+      alt="arrow down"
+      className="w-[18px] h-[28px] object-contain mt-[28px]"
+      >
+        
+      </motion.img>
     </motion.div>
   </section>
 );
