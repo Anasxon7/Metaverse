ed9a1de3c96f2c34078fef54a4f30d8298318cd3
Madaminov <muhammadanasxon6694@gmail.com>
2023-02-01 00:13:07 +0500

    add

--- sections/About.jsx
+++ sections/About.jsx
@@ -16,6 +16,7 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
+      <TypingText title="| About Metaversus" textStyles="text-center" />
 
     </motion.div>
   </section>
